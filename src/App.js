d60a3d198e36f42782bcb778a40d6ce9a572bb97
Adam Birnbaum <adambiren19@gmail.com>
2022-07-14 13:51:54 +0300

    small update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { useState } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 import Admin from './Components/Admin/Admin';
 import Home from './Components/Home/Home';
+import Stats from './Components/Stats/Stats';
 
 function App(props) {
-  
+
   let items = [];
 
   return (
     <div className="App">
-      <Admin items={items} />
-      <Home items={items} />
+      <BrowserRouter>
+        <Route path="/admin" exact>
+          <Admin items={items} />
+        </Route>
+        <Route path="/home" exact>
+          <Home items={items} />
+        </Route>
+        <Route path="/stats" exact>
+          <Stats />
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
