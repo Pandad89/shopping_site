2cef9e8909f21da7da4533856113923e157da619
Adam Birnbaum <adambiren19@gmail.com>
2022-07-14 22:59:04 +0300

    Finished building server and mongodb connection

--- routes/items.js
+++ routes/items.js
@@ -29,13 +29,28 @@ router.post('/', async (req, res) => {
 });
 
 //Delete Item
-router.delete('/:id', getReview, async (req, res) => {
+router.delete('/:id', getItem, async (req, res) => {
     try {
-        await res.review.remove();
-        res.json({ message: 'Review Deleted!' })
+        await res.item.remove();
+        res.json({ message: 'Item Deleted!' })
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
 })
+
+async function getItem(req, res, next) {
+    let item;
+    try {
+        item = await Item.findById(req.params.id);
+        if (item === null) {
+            return res.status(404).json({ message: 'Cannot find item' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.item = item;
+    next();
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const chalk = require('chalk');
 const express = require('express');
 const path = require('path');
 
+const itemRouter = require('./routes/items');
+
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+const { get } = require('http');
 
-mongoose.connect(process.env.DATABASE_URL, { userNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(chalk.inverse.red(error)));
 db.once('open', () => console.log(chalk.inverse.green('Connected to database')));
@@ -18,6 +21,12 @@ app.use(express.json());
 
 const publicDirectoryPath = path.join(__dirname, "client/build");
 app.use(express.static(publicDirectoryPath));
+
+app.use('/api/items', itemRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(chalk.inverse.green(`Server started on port ${port}`)));
